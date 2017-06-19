cc8683d4cb2184550e2c73285fc7a154b04e04d2
Maciej Gibas <gibasmaciej@gmail.com>
2017-06-18 21:53:50 -0400

    Creating challenges

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,40 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.createPlayer = functions.auth.user().onCreate(event => {
+  return admin.database()
+    .ref('/players')
+    .child(event.data.uid)
+    .set({
+      email: event.data.email,
+      displayName: event.data.displayName,
+      photoURL: event.data.photoURL
+    })
+});
+
+exports.createChallenge = functions.database.ref('/users/{userUid}/challenges/{challengeUid}')
+    .onWrite(event => {
+      let creatorUid = event.params.userUid;
+      let challengeUid = event.params.challengeUid;
+      let challenge = event.data.val();
+      if (event.data.previous.exists() || !event.data.exists() ||
+        challenge.creatorUid || challenge.playerUid === creatorUid) {
+        return;
+      }
+
+      challenge.createdDate = new Date();
+      challenge.creatorUid = creatorUid;
+
+      return Promise.all([
+        admin.database()
+          .ref(`/users/${challenge.playerUid}/challenges`)
+          .child(challengeUid)
+          .set(challenge),
+        admin.database()
+          .ref(`/users/${creatorUid}/challenges`)
+          .child(challengeUid)
+          .set(challenge)
+      ]);
+    });
