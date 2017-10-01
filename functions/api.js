5a370d7a1ecd8c468166d36c669532026a08ccd2
Maciej Gibas <gibasmaciej@gmail.com>
2017-10-01 19:53:38 -0400

    Added authorization

--- functions/api.js
+++ functions/api.js
@@ -11,23 +11,24 @@ class Api {
   }
 
   validateToken (req, res, next) {
-  //   if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer '))) {
-  //     return res.status(403).send('Unauthorized')
-  //   }
-  //
-  //   let idToken = req.headers.authorization.split('Bearer ')[1]
-  //
-  //   admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
-  //     req.user = decodedIdToken
-    next()
-  //   }).catch(error => {
-  //     console.error('Error while verifying Firebase ID token:', error)
-  //     return res.status(403).send('Unauthorized')
-  //   })
+    if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer '))) {
+      return res.status(403).send('Unauthorized')
+    }
+
+    this._admin.auth()
+      .verifyIdToken(req.headers.authorization.split('Bearer ')[1])
+      .then(user => {
+        req.user = user
+        return next()
+      })
+      .catch(error => {
+        console.error('Error while verifying Firebase ID token:', error)
+        return res.status(403).send('Unauthorized')
+      })
   }
   _configureRoutes () {
     this.handler.post('/corpos', (req, res) => {
-      res.status(200).send(`Hello Maciej`)
+      res.status(200).send(`Hello ${req.user.name}`)
     })
   }
 }
