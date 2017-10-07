8fea27a01934b62e9625f79d1a0da523aca62fdf
Maciej Gibas <gibasmaciej@gmail.com>
2017-10-07 19:20:50 -0400

    fixed validation

--- functions/api.js
+++ functions/api.js
@@ -76,7 +76,7 @@ class Api {
           let org = snap.val()
           if (!org) return res.status(400).send(`org ${req.params.name} does not exists`)
           if (req.user.email.split('@')[1] !== org.admin.autoAcceptDomain &&
-            (!req.body || !req.body.invitation.split('code=')[1] !== org.admin.invitationCode)) {
+            (!req.body || !req.body.invitation || !req.body.invitation.split('code=')[1] !== org.admin.invitationCode)) {
             return res.status(403).send(`Invalid invitation or domain`)
           }
           if (org.players[req.user.uid]) return res.sendStatus(200)
